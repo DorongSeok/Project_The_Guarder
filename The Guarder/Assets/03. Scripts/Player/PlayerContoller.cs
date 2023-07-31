105b6a9a97a7e7a8b8a1d82f48dbcf3f2851b0c4
DorongSeok <71745818+DorongSeok@users.noreply.github.com>
2023-08-01 06:34:53 +0900

    feat: Add Swipe PlayerMove in Android

--- The Guarder/Assets/03. Scripts/Player/PlayerContoller.cs
+++ The Guarder/Assets/03. Scripts/Player/PlayerContoller.cs
@@ -6,6 +6,15 @@ public class PlayerContoller : MonoBehaviour
 {
     private PlayerMoveInGrid playerMoveInGrid;
 
+    public float minSwipeLength = 200f;
+    Vector2 firstPressPos;
+    Vector2 secondPressPos;
+    Vector2 currentSwipe;
+    float tweakFactor = 0.5f;
+
+    private float swipeX;
+    private float swipeY;
+
     private void Awake()
     {
         playerMoveInGrid = GetComponent<PlayerMoveInGrid>();
@@ -13,9 +22,114 @@ public class PlayerContoller : MonoBehaviour
 
     private void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
+        bool isSwipe;
+        isSwipe = DetectSwipe();
 
-        playerMoveInGrid.MoveDirection = new Vector3(x, y, 0);
+        if(isSwipe == true)
+        {
+            playerMoveInGrid.MoveDirection = new Vector3(swipeX, swipeY, 0);
+        }
+        else
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            float y = Input.GetAxisRaw("Vertical");
+
+            playerMoveInGrid.MoveDirection = new Vector3(x, y, 0);
+        }
+    }
+
+
+
+    public bool DetectSwipe()
+    {
+        if (Input.touches.Length > 0)
+        {
+            Touch t = Input.GetTouch(0);
+
+            if (t.phase == TouchPhase.Began)
+            {
+                firstPressPos = new Vector2(t.position.x, t.position.y);
+            }
+
+            if (t.phase == TouchPhase.Ended)
+            {
+                secondPressPos = new Vector2(t.position.x, t.position.y);
+                currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+                // 스와이프 입력이 아닌 탭 입력인지 확인
+                if (currentSwipe.magnitude < minSwipeLength)
+                {
+                    return false;
+                }
+
+                currentSwipe.Normalize();
+
+                // Swipe up
+                if (currentSwipe.y > 0 && currentSwipe.x > 0 - tweakFactor && currentSwipe.x < tweakFactor)
+                {
+                    swipeX = 0;
+                    swipeY = 1;
+
+                    return true;
+                }
+                // Swipe down
+                else if (currentSwipe.y < 0 && currentSwipe.x > 0 - tweakFactor && currentSwipe.x < tweakFactor)
+                {
+                    swipeX = 0;
+                    swipeY = -1;
+
+                    return true;
+                }
+                // Swipe left
+                else if (currentSwipe.x < 0 && currentSwipe.y > 0 - tweakFactor && currentSwipe.y < tweakFactor)
+                {
+                    swipeX = -1;
+                    swipeY = 0;
+
+                    return true;
+                }
+                // Swipe right
+                else if (currentSwipe.x > 0 && currentSwipe.y > 0 - tweakFactor && currentSwipe.y < tweakFactor)
+                {
+                    swipeX = 1;
+                    swipeY = 0;
+
+                    return true;
+                }
+                // Swipe up left
+                else if (currentSwipe.y > 0 && currentSwipe.x < 0 )
+                {
+                    swipeX = -1;
+                    swipeY = 1;
+
+                    return true;
+                }
+                // Swipe up right
+                else if (currentSwipe.y > 0 && currentSwipe.x > 0 )
+                {
+                    swipeX = 1;
+                    swipeY = 1;
+
+                    return true;
+                }
+                // Swipe down left
+                else if (currentSwipe.y < 0 && currentSwipe.x < 0 )
+                {
+                    swipeX = -1;
+                    swipeY = -1;
+
+                    return true;
+                }
+                // Swipe down right
+                else if (currentSwipe.y < 0 && currentSwipe.x > 0 )
+                {
+                    swipeX = 1;
+                    swipeY = -1;
+
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
